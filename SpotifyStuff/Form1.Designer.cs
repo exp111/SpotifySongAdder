6052591384e97457dc4a4a71173a57face965db8
Exp <theexp111@gmail.com>
2018-06-19 15:27:51 +0200

    Added Icon (not mine; http://www.iconarchive.com/download/i98446/dakirby309/simply-styled/Spotify.ico)

--- SpotifyStuff/Form1.Designer.cs
+++ SpotifyStuff/Form1.Designer.cs
@@ -28,6 +28,7 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
 			this.button1 = new System.Windows.Forms.Button();
 			this.addSongURL = new System.Windows.Forms.TextBox();
 			this.playlistList = new System.Windows.Forms.ListBox();
@@ -162,6 +163,7 @@
 			this.Controls.Add(this.playlistList);
 			this.Controls.Add(this.addSongURL);
 			this.Controls.Add(this.button1);
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
 			this.Name = "Form1";
 			this.Text = "Spotify Song Adder";
 			((System.ComponentModel.ISupportInitialize)(this.addSongCount)).EndInit();
